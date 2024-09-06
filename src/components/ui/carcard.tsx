753ce96f6fdf90cbed8859db994f418ef435a0a2
Joaquin de la Iglesia <joaquindlaiglesia@gmail.com>
2024-09-06 12:01:19 -0300

    fix: some compatibility issues on carcard.tsx

--- src/components/ui/carcard.tsx
+++ src/components/ui/carcard.tsx
@@ -34,7 +34,12 @@ const CarCard: React.FC<CarCardProps> = ({
           href="#"
           className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
           prefetch={false}
-          onClick={() => document.getElementById("modal").showModal()}
+          onClick={() => {
+            const modal = document.getElementById("modal") as HTMLDialogElement;
+            if (modal) {
+              modal.showModal();
+            }
+          }}
         >
           Comprar
         </Link>
